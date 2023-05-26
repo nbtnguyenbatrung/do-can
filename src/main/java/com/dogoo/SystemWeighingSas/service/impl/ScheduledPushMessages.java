31f2349b0faf29d7d050ef16c139fea0b24ead66
trung <batrung255@gmail.com>
2023-05-26 10:06:02 +0700

    update

--- src/main/java/com/dogoo/SystemWeighingSas/service/impl/ScheduledPushMessages.java
+++ src/main/java/com/dogoo/SystemWeighingSas/service/impl/ScheduledPushMessages.java
@@ -1,4 +1,4 @@
-package com.dogoo.SystemWeighingSas.controllerPublic;
+package com.dogoo.SystemWeighingSas.service.impl;
 
 import com.dogoo.SystemWeighingSas.service.AccountService;
 import com.dogoo.SystemWeighingSas.service.WeighingStationService;
@@ -6,36 +6,34 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
-
-@Controller
-public class SocketScheduleController {
+@Service
+public class ScheduledPushMessages {
 
     private final AccountService accountService;
-    private final SimpMessagingTemplate template;
+    private SimpMessagingTemplate template;
     private final WeighingStationService weighingStationService;
-    public SocketScheduleController(AccountService accountService,
-                                    SimpMessagingTemplate template,
-                                    WeighingStationService weighingStationService) {
+
+    public ScheduledPushMessages(AccountService accountService,
+                                 SimpMessagingTemplate template,
+                                 WeighingStationService weighingStationService) {
         this.accountService = accountService;
         this.template = template;
         this.weighingStationService = weighingStationService;
     }
 
-
-    @MessageMapping("/public/dogoo/ws-message")
     @Scheduled(fixedRate = 30000)
     public void sendRole(@Payload String username) {
         template.convertAndSendToUser(username, "/public/dogoo/role",
                 accountService.getAccountByAccountId(Long.parseLong(username)));
     }
 
-    @MessageMapping("/public/dogoo/ws-message")
     @Scheduled(fixedRate = 30000)
     public void sendWeighingStation(@Payload String username) {
         template.convertAndSendToUser(username, "/public/dogoo/weighingStation",
                 weighingStationService.getWeighingStationByCustomerKey(username));
     }
+
 
 }
